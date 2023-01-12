3e008207a5983f08f0866e75d7b611cc991784b9
socoolheeya <lwh0102@naver.com>
2023-01-13 01:08:06 +0900

    Initial commit

---
+++ src/main/java/com/sweeti/jooqtest/JooqTestApplication.java
@@ -0,0 +1,13 @@
+package com.sweeti.jooqtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JooqTestApplication.class, args);
+    }
+
+}
